bbf07090e7c824d5c3012217bc960fcea10669d7
Melanie Buck <melaniebuck93@gmail.com>
2021-04-08 21:38:40 +0200

    working on task router

---
+++ client/src/views/CreateTask.jsx
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import { createTask } from './../services/task';
+
+class CreateTask extends Component {
+  state = {
+    name: '',
+    assignment: '',
+    description: '',
+    price: '',
+    hourOfWork: '',
+    status: []
+    // pictures: '',
+  };
+
+  handleFormSubmission = async (event) => {
+    event.preventDefault();
+    const {
+      name,
+      assignment,
+      description,
+      price,
+      hourOfWork,
+      status
+    } = this.state;
+    const data = {
+      name,
+      assignment,
+      description,
+      price,
+      hourOfWork,
+      status
+    };
+    const body = new FormData();
+    for (let key in data) {
+      const value = data[key];
+      if (value instanceof Array) {
+        for (let item of value) {
+          body.append(key, item);
+        }
+      } else {
+        body.append(key, value);
+      }
+    }
+    const pet = await createTask(body);
+    this.props.history.push(`/task/${task._id}`);
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFileInputChange = (event) => {
+    const { name, files } = event.target;
+    const arrayOfFiles = [];
+    for (const file of files) arrayOfFiles.push(file);
+    this.setState({
+      [name]: arrayOfFiles
+    });
+  };
+
+  render() {
+    return (
+      <main>
+        <header>
+          <h1>Add a task</h1>
+        </header>
+        <form onSubmit={this.handleFormSubmission}>
+          <label htmlFor="input-name">Name</label>
+          <input
+            id="input-name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            required
+          />
+
+          <div className="row">
+            <div className="col">
+              <label htmlFor="input-species">Type of Assignment</label>
+              <select
+                id="input-assignment"
+                name="assignment"
+                value={this.state.assignment}
+                onChange={this.handleInputChange}
+                required
+              >
+                <option value="" disabled>
+                  Assignment
+                </option>
+                <option value="single_task">Single Task</option>
+                <option value="project">Project</option>
+              </select>
+            </div>
+          </div>
+
+          <label htmlFor="input-description">Description</label>
+          <textarea
+            id="input-description"
+            name="description"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+          />
+
+          <div className="col">
+            <label htmlFor="input-typeOfWork">Type of Work</label>
+            <select
+              id="input-typeOfWork"
+              name="typeOfWork"
+              value={this.state.gender}
+              onChange={this.handleInputChange}
+            >
+              <option value="">Non-specified</option>
+              <option value="physical">Physical</option>
+              <option value="administrative">Administrative</option>
+            </select>
+          </div>
+
+        <div className="row">
+          <div className="col">
+            <label htmlFor="input-price">
+              How much can I pay you for this in EUR
+            </label>
+            <input
+              id="input-price"
+              name="price"
+              type="number"
+              placeholder="Price"
+              min="0"
+              value={this.state.price}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+
+          <label htmlFor="input-hourOfWork">
+            Approximate amount of hours required to complete the job
+          </label>
+          <input
+            id="input-hourOfWork"
+            name="hourOfWork"
+            type="text"
+            placeholder="Amount Of Hours"
+            value={this.state.hourOfWork}
+            onChange={this.handleInputChange}
+            required
+          />
+
+          <label htmlFor="input-pictures">Pictures</label>
+          <input
+            id="input-pictures"
+            type="file"
+            name="pictures"
+            multiple
+            required
+            onChange={this.handleFileInputChange}
+          />
+
+          <div className="col">
+            <label htmlFor="input-status">Task Status</label>
+            <select
+              id="input-status"
+              name="status"
+              value={this.state.status}
+              onChange={this.handleInputChange}
+              required
+            >
+              <option value="" disabled>
+                Status
+              </option>
+              <option value="open">Open</option>
+              <option value="in_process">In Process</option>
+              <option value="closed">Closed</option>
+            </select>
+          </div>
+
+          <button>Create Pet</button>
+        </form>
+      </main>
+    );
+  }
+
+export default CreatePet;
