2bb7b18e19327bf8c66f9bb88f1362dbe5bb78e8
Dardannya Patente <dardannyapatente@hotmail.com>
2021-04-23 09:50:16 +0200

    fixed syntax on messenger.jsx file

--- server/routes/messenger.js
+++ server/routes/messenger.js
@@ -31,7 +31,7 @@ router.get('/', routeGuard, (req, res, next) => {
              ]
            }
          ]
-       }         .populate('senderId')
+       })        .populate('senderId')
          .populate('receiverId');
      })
     .then((messages) => {
